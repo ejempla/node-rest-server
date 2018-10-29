e3a1e22f42dea6b3df997d6a1b853ec098ee0fb7
javier <javier.lopez@rjconsultores.com.mx>
2018-10-29 00:11:25 -0600

    restServert - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,5 +3,21 @@ definimos el puerto
 
 */
 
-process.env.PORT = process.env.PORT || 3001;
+process.env.PORT = process.env.PORT || 3000;
+/*
+ENTORNO
+*/
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:javier1994@ds021922.mlab.com:21922/cafe-user';
+}
+
+
+process.env.URLDB = urlDB;
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,44 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-    res.json({ accion: 'get Usuario' })
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', (req, res) => {
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje : 'el nombre es nesesario'
-        });
+mongoose.connect(process.env.URLDB, (error, res) => {
+    if (error) {
+        throw error;
     } else {
-        res.json({
-            body,
-            nombre: body.nombre,
-            edad: body.edad,
-            accion: "post Usuario"
-        });
+        console.log('base de datos ONLINE');
+
     }
-
-
 });
 
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id,
-        accion: 'put Usuario'
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json({ accion: 'delet Usuario' });
-});
 
 app.listen(process.env.PORT, () => {
-    console.log(`activo en puerto ${ process.env.PORT }`);
+    console.log(`activo en puerto ${process.env.PORT}`);
 
 });
